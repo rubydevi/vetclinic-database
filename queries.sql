09fe7849b4a9519ab1bf5d22634a2411f2d0a536
iamrubydevi <ariesabyss@gmail.com>
2023-08-29 15:00:17 +0530

    Delete queries

--- queries.sql
+++ queries.sql
@@ -222,7 +222,3 @@ JOIN species s ON s.id = a.species_id
 WHERE v.name = 'Maisy Smith'
 GROUP BY s.name
 LIMIT 1;
-
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
-EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
